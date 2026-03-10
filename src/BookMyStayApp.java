bc1aee1143928695236c976628ea6b8cba0345a3
pb3363-art <pb3363@srmist.edu.in>
2026-03-10 15:46:31 +0530

    UC1: Add welcome message module

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,14 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+
+        String appName = "Hotel Booking System";
+        String version = "v1.0";
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" Version: " + version);
+        System.out.println("Application started successfully.");
+        System.out.println("Application shutting down...");
+    }
+}
