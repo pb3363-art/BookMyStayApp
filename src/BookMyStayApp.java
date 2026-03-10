50d050b64a8d51c6f6479d4d3c69908364c7dc31
pb3363-art <pb3363@srmist.edu.in>
2026-03-10 15:53:02 +0530

    UC2: Basic Room Types and Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,24 @@
-public class BookMyStayApp {
-    public static void main(String[] args) {
+package hotel;
 
+public abstract class BookMyStayApp {
+
+    protected int roomNumber;
+    protected String roomType;
+    protected double price;
+    protected boolean available;
+
+    public BookMyStayApp (int roomNumber, String roomType, double price, boolean available) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.price = price;
+        this.available = available;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Number: " + roomNumber);
+        System.out.println("Type: " + roomType);
+        System.out.println("Price: " + price);
+        System.out.println("Available: " + available);
+        System.out.println();
     }
 }
